14285cf951dcf464d273587a826b4ba9d1cec60c
cc <aa@qq.com>
2023-05-30 14:57:05 +0800

    feat: provider和inject的demo

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,35 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  // constructor(private readonly appService: AppService) {}
+
+  // @Inject(AppService)
+  // private readonly appService: AppService;
+
+  // @Inject('app_service')
+  private readonly appService: AppService;
+
+  @Inject('userInfo')
+  private readonly userInfo: { name: string; age: number };
+
+  @Inject('userInfo2')
+  private readonly userInfo2: { name: string; desc: string };
+
+  @Inject('userInfo5')
+  private readonly userInfo5: { name: string; desc: string };
+
+  @Inject('userInfo4')
+  private readonly userInfo4: { name: string; age: number };
 
   @Get()
   getHello(): string {
     debugger;
+    console.log(this.userInfo);
+    console.log(this.userInfo2);
+    console.log(this.userInfo5);
+    console.log(this.userInfo4);
     return this.appService.getHello();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,61 @@ import { AppService } from './app.service';
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    // {
+    //   provide: AppService,
+    //   useClass: AppService, // 完整写法
+    // },
+    // {
+    //   provide: 'app_service', //inject 时的token
+    //   useClass: AppService,
+    // },
+    {
+      provide: 'userInfo',
+      useValue: {
+        // 静态的值
+        name: 'cc',
+        age: 18,
+      },
+    },
+    {
+      provide: 'userInfo2',
+      useFactory() {
+        return {
+          name: 'dd',
+          desc: 'desc==',
+        };
+      },
+    },
+    {
+      provide: 'userInfo3',
+      useFactory(user: { name: string }, appService: AppService) {
+        return {
+          name: user.name,
+          desc: appService.getHello(),
+        };
+      },
+      inject: ['userInfo', AppService],
+    },
+    {
+      provide: 'userInfo5',
+      async useFactory() {
+        await new Promise((r) => {
+          setTimeout(() => {
+            r(200);
+          }, 2000);
+        });
+        return {
+          name: 'ffff',
+          desc: 'fff=>desc',
+        };
+      },
+    },
+    {
+      provide: 'userInfo4',
+      useExisting: 'userInfo', // 给userInfo起个别名
+    },
+  ],
 })
 export class AppModule {}
